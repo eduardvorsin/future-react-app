51a4de261c12abe8093e2418cbaac2e634a04fa4
eduardvorsin <eduardvorsin2019@yandex.kz>
2023-03-24 14:50:21 +0600

    refactor(button): add an iconButton prop

--- src/components/UI/Button/Button.tsx
+++ src/components/UI/Button/Button.tsx
@@ -5,17 +5,18 @@ type BaseButtonProps = Partial<React.ButtonHTMLAttributes<HTMLButtonElement>>;
 interface ButtonProps extends BaseButtonProps {
   children: React.ReactNode,
   className?: string,
+  iconButton?: boolean
   onClick?: React.MouseEventHandler<HTMLButtonElement>
 }
 
 const Button: React.FC<ButtonProps> = ({
   children,
+  iconButton,
   className,
   onClick,
 }) => {
   const buttonClasses = [
-    'button',
-    classes.button,
+    iconButton ? classes['button--icon-button'] : classes.button,
     className,
   ].join(' ');
   return (
