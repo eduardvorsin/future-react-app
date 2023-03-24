1e343f222c35bf76e796c7f725920748030be872
eduardvorsin <eduardvorsin2019@yandex.kz>
2023-03-24 14:51:16 +0600

    feat(searchbar): create a new component

---
+++ src/components/UI/SearchBar/SearchBar.tsx
@@ -0,0 +1,91 @@
+import React from 'react';
+import Button from '../Button/Button';
+import SearchIcon from '../../../assets/images/icons/find.svg';
+import ClearIcon from '../../../assets/images/icons/clear.svg';
+import classes from './SearchBar.module.css';
+
+type BaseSearchBarProps = Partial<React.InputHTMLAttributes<HTMLInputElement>>;
+
+interface SearchBarProps extends BaseSearchBarProps {
+  className?: string,
+  value: string | number,
+  onClear: React.MouseEventHandler<HTMLButtonElement>,
+  onChange: React.ChangeEventHandler<HTMLInputElement>,
+  onSearch: () => void,
+}
+
+const SearchBar: React.FC<SearchBarProps> = ({
+  className,
+  value,
+  onClear,
+  onChange,
+  onSearch,
+}) => {
+  const SearchFormClasses = [
+    classes['search-form'],
+    classes['search-form__search-bar'],
+  ].join(' ');
+
+  const SearchBarClasses = [
+    className,
+    classes['search-bar'],
+  ].join(' ');
+
+  const keydownHandler: React.KeyboardEventHandler<HTMLInputElement> = (e) => {
+    if (e.key === 'Enter') {
+      onSearch();
+    }
+  };
+
+  const searchSubmitHandler: React.FormEventHandler<HTMLFormElement> = (e) => {
+    e.preventDefault();
+    if (value.toString().length < 1) {
+      return;
+    }
+
+    onSearch();
+  };
+
+  return (
+    <form
+      className={SearchFormClasses}
+      onSubmit={searchSubmitHandler}
+    >
+      <div
+        className={SearchBarClasses}
+        role='search'
+      >
+        <label
+          htmlFor='search-bar'
+          className={`${classes['search-bar__label']} sr-only`}
+        >
+          Search bar
+        </label>
+        <input
+          id='search-bar'
+          className={classes['search-bar__input']}
+          onChange={onChange}
+          onKeyDown={keydownHandler}
+          value={value}
+          placeholder='books search'
+        />
+        <Button
+          iconButton
+          className={classes['search-bar__close']}
+          onClick={onClear}
+        >
+          <ClearIcon />
+        </Button>
+        <Button
+          iconButton
+          className={classes['search-bar__find']}
+          type='submit'
+        >
+          <SearchIcon />
+        </Button>
+      </div>
+    </form>
+  );
+};
+
+export default SearchBar;
