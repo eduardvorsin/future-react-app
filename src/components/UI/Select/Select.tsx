da0d9b46658fad7f61c6d38c53379927f79fe129
eduardvorsin <eduardvorsin2019@yandex.kz>
2023-03-28 16:25:53 +0600

    feat(select): create a new component

---
+++ src/components/UI/Select/Select.tsx
@@ -0,0 +1,68 @@
+import React, { SelectHTMLAttributes } from 'react';
+import classes from './Select.module.css';
+
+type SelectOption = {
+  value: string,
+  label: string,
+}
+type SelectBaseProps = Partial<SelectHTMLAttributes<HTMLSelectElement>>;
+interface SelectProps extends SelectBaseProps {
+  className?: string,
+  labelText: string,
+  id: string,
+  options: SelectOption[],
+  value: string
+  onChange: React.ChangeEventHandler<HTMLSelectElement>,
+}
+
+const Select: React.FC<SelectProps> = ({
+  className,
+  labelText,
+  id,
+  options,
+  value,
+  onChange,
+}) => {
+  const selectClasses = [
+    className,
+    classes.select,
+  ].join(' ');
+
+  const selectChangeHandler: React.ChangeEventHandler<HTMLSelectElement> = (e) => {
+    onChange(e);
+  };
+
+  return (
+    <div
+      className={selectClasses}
+    >
+      <label
+        className={classes.select__label}
+        htmlFor={id}
+      >
+        {labelText}
+      </label>
+      <div
+        className={classes['select-wrapper']}
+      >
+        <select
+          className={classes['select-wrapper__select']}
+          value={value}
+          onChange={selectChangeHandler}
+          id={id}
+        >
+          {options.map((option) => (
+            <option
+              key={option.value}
+              value={option.value}
+            >
+              {option.label}
+            </option>
+          ))}
+        </select>
+      </div>
+    </div>
+  );
+};
+
+export default Select;
