530205cedbc2f33a2474843996686874efa45854
eduardvorsin <eduardvorsin2019@yandex.kz>
2023-03-28 16:20:53 +0600

    feat(booksapi): add the BooksAPI class

---
+++ src/API/BooksAPI.ts
@@ -0,0 +1,49 @@
+import { getPageIndex } from '../helpers/helpers';
+
+export type getBooksOptions = {
+  page: number,
+  name: string,
+  sortOrder: string,
+  category: string,
+}
+
+export default class BooksAPI {
+  private static url = 'https://www.googleapis.com/books/v1/volumes';
+
+  static async getBooks(options: getBooksOptions) {
+    const {
+      name,
+      sortOrder = 'relevance',
+      category = 'all',
+      page,
+    } = options;
+
+    const currentCategory = category === 'all' ? '' : `+subject:${category}`;
+    const startIndex = getPageIndex(page);
+
+    const searchParams = [
+      `q=intitle:${name}${currentCategory}`,
+      `orderBy=${sortOrder}`,
+      'fields=totalItems,items(id,volumeInfo/authors,volumeInfo/title,volumeInfo/description,volumeInfo/categories,volumeInfo/imageLinks)',
+      'langRestrict=ru',
+      'maxResults=40',
+      `startIndex=${startIndex}`,
+      `key=${process.env.API_KEY as string}`,
+    ].join('&');
+
+    const response = await fetch(`${BooksAPI.url}?${searchParams}`);
+
+    return response;
+  }
+
+  static async getDefiniteBook(id: string) {
+    const searchParams = [
+      'fields=id,volumeInfo(authors,title,description,categories,imageLinks)',
+      `key=${process.env.API_KEY as string}`,
+    ].join('&');
+
+    const response = await fetch(`${BooksAPI.url}/${id}?${searchParams}`);
+
+    return response;
+  }
+}
