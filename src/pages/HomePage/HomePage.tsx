b1deec7affd5a7df161e8a25bac95e4e15a26d6b
eduardvorsin <eduardvorsin2019@yandex.kz>
2023-03-28 16:27:13 +0600

    feat(homepage): create a new component

---
+++ src/pages/HomePage/HomePage.tsx
@@ -0,0 +1,211 @@
+import React, { useState } from 'react';
+import { useNavigate, useOutlet } from 'react-router-dom';
+import useAppDispatch from '../../hooks/useAppDispatch/useAppDispatch';
+import { BookCategories, BookSortOrder } from '../../store/bookSlice/bookSlice';
+import fetchBooks from '../../store/thunks/fetchBooks/fetchBooks';
+import SearchBar from '../../components/UI/SearchBar/SearchBar';
+import Select from '../../components/UI/Select/Select';
+import Title from '../../components/UI/Title/Title';
+import classes from './HomePage.module.css';
+import useAppSelector from '../../hooks/useAppSelector/useAppSelector';
+import BooksCount from '../../components/BL/BooksCount/BooksCount';
+import BooksList from '../../components/BL/BooksList/BooksList';
+import Spinner from '../../components/UI/Spinner/Spinner';
+import Error from '../../components/UI/Error/Error';
+import Button from '../../components/UI/Button/Button';
+
+type CategoryOption = {
+  value: BookCategories,
+  label: string
+}
+
+type SorterOrderOption = {
+  value: BookSortOrder,
+  label: string
+}
+
+const categoriesOptions: CategoryOption[] = [
+  {
+    value: 'all',
+    label: 'All',
+  },
+  {
+    value: 'art',
+    label: 'Art',
+  },
+  {
+    value: 'biography',
+    label: 'Biography',
+  },
+  {
+    value: 'computers',
+    label: 'Computers',
+  },
+  {
+    value: 'history',
+    label: 'History',
+  },
+  {
+    value: 'medical',
+    label: 'Medical',
+  },
+  {
+    value: 'poetry',
+    label: 'Poetry',
+  },
+];
+
+const sortOrderOptions: SorterOrderOption[] = [
+  {
+    value: 'relevance',
+    label: 'Relevance',
+  },
+  {
+    value: 'newest',
+    label: 'Newest',
+  },
+];
+
+const HomePage = () => {
+  const [searchValue, setSearchValue] = useState<string>('');
+  const [category, setCategory] = useState<string>(categoriesOptions[0].value);
+  const [sortOrder, setSortOrder] = useState<string>(sortOrderOptions[0].value);
+
+  const dispatch = useAppDispatch();
+  const outlet = useOutlet();
+  const navigate = useNavigate();
+
+  const { data, status, error } = useAppSelector((state) => state.books);
+
+  const changeHandler: React.ChangeEventHandler<HTMLInputElement> = (e) => {
+    setSearchValue(e.target.value);
+  };
+
+  const searchHandler: () => void = () => {
+    dispatch(fetchBooks({
+      page: 0,
+      name: searchValue,
+      sortOrder,
+      category,
+    }));
+    navigate('/');
+  };
+
+  const clearHandler: () => void = () => {
+    setSearchValue('');
+  };
+
+  const sortOrderChangeHandler: React.ChangeEventHandler<HTMLSelectElement> = (e) => {
+    setSortOrder(e.currentTarget.value);
+  };
+
+  const categoryChangeHandler: React.ChangeEventHandler<HTMLSelectElement> = (e) => {
+    setCategory(e.currentTarget.value);
+  };
+
+  const loadMoreBooks = () => {
+    dispatch(fetchBooks({
+      page: data.page + 1,
+      name: searchValue,
+      sortOrder,
+      category,
+    }));
+    navigate('/');
+  };
+
+  const currentError = error || 'failed to load data about books try again';
+
+  return (
+    <div
+      className={`${classes['home-page']}`}
+    >
+      <section
+        className={`${classes['home-page__hero']} ${classes['hero-section']}`}
+      >
+        <div
+          className={`${classes['hero-section__container']} container`}
+
+        >
+          <Title
+            className={classes['hero-section__title']}
+            variant='light'
+            level={2}
+          >
+            Search for books
+          </Title>
+
+          <div
+            className={classes['hero-section__content']}
+          >
+            <SearchBar
+              className={classes['hero-section__search']}
+              value={searchValue}
+              onClear={clearHandler}
+              onSearch={searchHandler}
+              onChange={changeHandler}
+            />
+
+            <Select
+              className={classes['hero-section__category-select']}
+              value={category}
+              labelText='Categories'
+              id='categories'
+              onChange={categoryChangeHandler}
+              options={categoriesOptions}
+            />
+
+            <Select
+              className={classes['hero-section__sort-select']}
+              value={sortOrder}
+              labelText='Sorting by'
+              id='sortingBy'
+              onChange={sortOrderChangeHandler}
+              options={sortOrderOptions}
+            />
+          </div>
+        </div>
+      </section>
+
+      <section
+        className={classes['books-section']}
+      >
+        <div
+          className={`${classes['books-section__container']} container`}
+        >
+          {status === 'loading' && (
+            <Spinner
+              size={200}
+            />
+          )}
+
+          {status === 'rejected' && (
+            <Error
+              className={classes['books-section__error']}
+              message={currentError}
+            />
+          )}
+
+          {status === 'resolved' && outlet === null && (
+            <>
+              <BooksCount>Найдено {data.totalItems} книг</BooksCount>
+              <BooksList
+                data={data.items}
+              />
+              {data.totalItems - data.items.length > 0 && (
+                <Button
+                  className={classes['books-section__button']}
+                  onClick={loadMoreBooks}
+                >
+                  Смотреть еще
+                </Button>
+              )}
+            </>
+          )}
+          {outlet}
+        </div>
+      </section>
+    </div>
+  );
+};
+
+export default HomePage;
