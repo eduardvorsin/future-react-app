c421ff48364caef3ac1ed1211d73f51a9f49f33c
eduardvorsin <eduardvorsin2019@yandex.kz>
2023-07-04 16:22:58 +0600

    feat(booktypes): add field types for api books

--- src/API/bookTypes.ts
+++ src/API/bookTypes.ts
@@ -1,3 +1,39 @@
+export interface VolumeInfo {
+  authors: string[],
+  categories: string[],
+  description: string,
+  language: string,
+  maturityRating: 'NOT_MATURE' | 'MATURE',
+  pageCount: number,
+  publishedDate: string,
+  publisher: string,
+  title: string,
+  industryIdentifiers: {
+    type: string,
+    identifier: number,
+  }[],
+  imageLinks: {
+    smallThumbnail: string,
+    thumbnail: string,
+    extraLarge?: string,
+    large?: string,
+    medium?: string,
+    small?: string,
+  },
+}
+
+export interface AccessInfo {
+  webReaderLink: string,
+}
+
+export interface SaleInfo {
+  buyLink: string,
+  isEbook: boolean,
+  retailPrice: {
+    amount: number,
+    currencyCode: string,
+  },
+}
 export type BookCategories = 'all' | 'art' | 'biography' | 'computers' | 'history' | 'medical' | 'poetry';
 export type BookSortOrder = 'relevance' | 'newest';
 
