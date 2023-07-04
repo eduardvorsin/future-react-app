4df2632ccdf2d8b308c489b4da959586a3d2278d
eduardvorsin <eduardvorsin2019@yandex.kz>
2023-07-04 16:35:53 +0600

    refactor(ibook): update the iBook and IBookPartial types

--- src/model/IBook.ts
+++ src/model/IBook.ts
@@ -1,15 +1,19 @@
-export type IBook = {
+export type IBook = VolumeInfo & AccessInfo & SaleInfo;
+export type IBookPartial = {
   id: string,
-  authors: string[],
-  categories: string[],
-  description: string,
+  maturityRating: 'NOT_MATURE' | 'MATURE',
   title: string,
-  imageLinks: {
+  authors?: string[],
+  imageLinks?: {
     smallThumbnail: string,
     thumbnail: string,
     extraLarge?: string,
     large?: string,
     medium?: string,
     small?: string,
-  }
-}
+  },
+  retailPrice?: {
+    amount: number,
+    currencyCode: string,
+  },
+};
