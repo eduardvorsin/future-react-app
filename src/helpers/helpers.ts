5a89a03ff2d10adc8f88441af4c4b64c3b761f1c
eduardvorsin <eduardvorsin2019@yandex.kz>
2023-07-04 15:31:03 +0600

    feat(helpers.ts): add functions for saving and changing scroll position data

--- src/helpers/helpers.ts
+++ src/helpers/helpers.ts
@@ -4,3 +4,13 @@ export const getPageIndex = (page: number) => {
   const booksPerPage = 40;
   return page * booksPerPage;
 };
+
+export const saveVerticalScrollPosition = (): void => {
+  sessionStorage.setItem('scrollY', window.scrollY.toString());
+};
+
+export const getVerticalScrollPosition = (): string | null => sessionStorage.getItem('scrollY');
+
+export const resetVerticalScrollPosition = (): void => {
+  sessionStorage.setItem('scrollY', '0');
+};
