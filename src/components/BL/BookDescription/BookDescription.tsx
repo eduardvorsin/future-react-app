f795760bdc93ef4360756646970e5e336adc6a87
eduardvorsin <eduardvorsin2019@yandex.kz>
2023-07-05 16:04:16 +0600

    feat(bookdescription): change the structure of the component

--- src/components/BL/BookDescription/BookDescription.tsx
+++ src/components/BL/BookDescription/BookDescription.tsx
@@ -1,72 +1,115 @@
-import React, { useEffect } from 'react';
-import { useParams } from 'react-router-dom';
-import useAppDispatch from '../../../hooks/useAppDispatch/useAppDispatch';
+import React from 'react';
 import useAppSelector from '../../../hooks/useAppSelector/useAppSelector';
-import fetchDefiniteBook from '../../../store/thunks/fetchDefiniteBook/fetchDefiniteBook';
 import Title from '../../UI/Title/Title';
 import classes from './BookDescription.module.css';
 import PlaceholderImage from '../../../assets/images/placeholder388x613-ru.jpeg';
+import Link from '../../UI/Link/Link';
+import OpenBookIcon from '../../../assets/images/icons/open-book.svg';
+import currencyFormat from '../../../localization/formatting/numbers';
+import CharacteristicList from '../../UI/CharacteristicList/CharacteristicList';
 
 const BookDescription = () => {
-  const { id } = useParams();
+  const { currentBook } = useAppSelector((state) => state.books);
+  const categories = currentBook?.categories?.join(', ') ?? 'Неизвестно';
+  const imageSrc = currentBook?.imageLinks?.medium ?? PlaceholderImage;
+  const description = currentBook?.description ?? 'Описание к данной книге отсутствует';
+  const title = currentBook?.title ?? 'Неизвестно';
+  const previewLink = currentBook?.webReaderLink ?? '';
 
-  const dispatch = useAppDispatch();
-  const { data } = useAppSelector((state) => state.books);
+  let price = currentBook?.retailPrice?.amount ?? 'Нет в наличии';
 
-  useEffect(() => {
-    const fetchBookDescription = async () => {
-      if (id) {
-        dispatch(fetchDefiniteBook(id));
-      }
-    };
-
-    fetchBookDescription();
-  }, [id]);
-
-  const categories = data.currentBook?.categories?.join(', ') ?? 'Неизвестно';
-  const authors = data.currentBook?.authors?.join(', ') ?? 'Неизвестно';
-  const imageSrc = data.currentBook?.imageLinks?.medium ?? PlaceholderImage;
-
+  if (typeof price === 'number') {
+    price = currencyFormat(price, {
+      currency: currentBook?.retailPrice?.currencyCode,
+    });
+  }
   return (
     <div
       className={classes['book-description']}
     >
+      <Title
+        className={classes['book-description__main-title']}
+        component='h2'
+        variant='dark'
+        level={2}
+      >
+        {title}
+      </Title>
       <div
         className={classes['book-description__img-box']}
       >
         <img
           src={imageSrc}
           className={classes['book-description__img']}
-          alt={`${data.currentBook?.title} book cover`}
+          alt={`${title} book cover`}
+          width={435}
+          height={632}
         />
       </div>
       <div
         className={classes['book-description__content']}
       >
+        <Link
+          className={classes['book-description__preview-link']}
+          icon={OpenBookIcon}
+          path={previewLink}
+        >
+          Читать фрагмент книги
+        </Link>
         <p
           className={classes['book-description__category']}
         >
-          Категории: {categories}
+          Категории: <span>{categories}</span>
         </p>
+        {currentBook && (
+          <>
+            <Title
+              className={classes['book-description__title']}
+              component='h3'
+              variant='dark'
+              level={4}
+            >
+              Характеристики
+            </Title>
+
+            <CharacteristicList
+              bookData={currentBook}
+            />
+          </>
+        )}
+      </div>
+      <div className={classes['book-description__buying']}>
+        <p
+          className={classes['book-description__price']}
+        >
+          {price}
+        </p>
+        <Link
+          path={currentBook?.buyLink}
+          variant='tertiary'
+          className={classes['book-description__buying-btn']}
+        >
+          Купить
+        </Link>
+      </div>
+      <div
+        className={classes['book-description__describing']}
+      >
         <Title
           className={classes['book-description__title']}
           component='h3'
           variant='dark'
           level={4}
         >
-          {data.currentBook?.title}
+          Описание
         </Title>
-        <p
-          className={classes['book-description__authors']}
-        >
-          Авторы: {authors}
-        </p>
         <p
           className={classes['book-description__text']}
         >
-          {data.currentBook?.description}
+          {description}
         </p>
       </div>
+
     </div>
   );
 };
