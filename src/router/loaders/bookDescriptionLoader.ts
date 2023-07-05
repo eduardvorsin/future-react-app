328d73b6e11a72264242b4cc5021e982bf4c2121
eduardvorsin <eduardvorsin2019@yandex.kz>
2023-07-05 15:45:30 +0600

    feat(bookdescriptionloader): create a loader for the  bookDescription

---
+++ src/router/loaders/bookDescriptionLoader.ts
@@ -0,0 +1,18 @@
+import { LoaderFunctionArgs, ParamParseKey, Params } from 'react-router-dom';
+import fetchDefiniteBook from '../../store/thunks/fetchDefiniteBook/fetchDefiniteBook';
+import { bookDescriptionPathName } from '../AppRouter/AppRouter';
+import store from '../../store/store';
+
+interface bookDescriptionLoaderArgs extends LoaderFunctionArgs {
+  params: Params<ParamParseKey<bookDescriptionPathName>>
+}
+
+const bookDescriptionLoader = async ({ params }: bookDescriptionLoaderArgs) => {
+  if (params.id) {
+    store.dispatch(fetchDefiniteBook(params.id));
+  }
+
+  return null;
+};
+
+export default bookDescriptionLoader;
