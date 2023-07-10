485cc60e4a78a4b5f1efd35980a372b4986fddd5
eduardvorsin <eduardvorsin2019@yandex.kz>
2023-07-10 11:58:36 +0600

    feat(app): add the ability to switch the dark theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,31 @@
-import React from 'react';
+import React, { useCallback, useEffect, useMemo, useState } from 'react';
 import AppRouter from './router/AppRouter/AppRouter';
+import { ThemeContext, defaultTheme } from './contexts/ThemeContext';
 
 export default function App() {
+  const [theme, setTheme] = useState<'light' | 'dark'>(defaultTheme);
+  const toggleTheme = useCallback(() => {
+    setTheme((prevTheme) => (prevTheme === 'light' ? 'dark' : 'light'));
+  }, [theme]);
+
+  const themeContextValue = useMemo(() => ({
+    value: theme,
+    toggleTheme,
+  }), [theme, toggleTheme]);
+
+  useEffect(() => {
+    document.documentElement.setAttribute('data-theme', theme);
+    return () => {
+      document.documentElement.removeAttribute('data-theme');
+    };
+  }, [theme]);
+
   return (
-    <AppRouter />
+    <ThemeContext.Provider value={themeContextValue}>
+      <div className='app'>
+        <AppRouter />
+      </div>
+
+    </ThemeContext.Provider>
   );
 }
