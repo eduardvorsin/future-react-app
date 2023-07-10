24081d2c5f7b0b040e062d575c14a9c6e20a4a49
eduardvorsin <eduardvorsin2019@yandex.kz>
2023-07-10 11:50:13 +0600

    feat(switcher): create a new component

---
+++ src/components/UI/Switcher/Switcher.tsx
@@ -0,0 +1,51 @@
+import React, { ChangeEventHandler, FC } from 'react';
+import classes from './Switcher.module.css';
+
+type SwitcherProps = {
+  className?: string,
+  labelText: string
+  checked: boolean,
+  name: string,
+  value: string[],
+  onChange: ChangeEventHandler<HTMLInputElement>,
+}
+
+const Switcher: FC<SwitcherProps> = ({
+  className,
+  labelText,
+  checked,
+  name,
+  value,
+  onChange,
+}) => (
+  <fieldset
+    className={`${classes.switcher} ${className}`}
+  >
+    <legend
+      className={`${classes.switcher__label} sr-only`}
+    >
+      {labelText}
+    </legend>
+    <input
+      className={classes.switcher__radio}
+      name={name}
+      role='switch'
+      type='radio'
+      value={value[0]}
+      checked={checked}
+      onChange={onChange}
+    />
+    <input
+      className={classes.switcher__radio}
+      name={name}
+      role='switch'
+      type='radio'
+      value={value[1]}
+      checked={!checked}
+      onChange={onChange}
+    />
+    <span className={classes.switcher__status}></span>
+  </fieldset>
+);
+
+export default Switcher;
