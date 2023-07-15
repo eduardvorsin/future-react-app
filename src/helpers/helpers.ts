2c2d3f1b6a8a1820a6895ef0b92ac962df583f0b
eduardvorsin <eduardvorsin2019@yandex.kz>
2023-07-15 13:28:28 +0600

    feat(helpers): create createOptionsFromValues helper

--- src/helpers/helpers.ts
+++ src/helpers/helpers.ts
@@ -137,3 +137,18 @@ export const getColorTheme = (): 'dark' | 'light' => {
 
   return currentTheme as 'dark' | 'light';
 };
+
+export const createOptionsFromValues = (values: string[], localizationPath: string) => {
+  const labels: string[] = values.map((value) => i18n.t(`${localizationPath}.${value}`, { ns: 'homePage' }));
+
+  const options = values.map((value, index) => {
+    const option = {
+      value,
+      label: labels[index],
+    };
+
+    return option;
+  });
+
+  return options;
+};
