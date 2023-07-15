49d3e978b6f576731d05d789dd05ee3002ab7008
eduardvorsin <eduardvorsin2019@yandex.kz>
2023-07-15 13:36:01 +0600

    feat(notfoundpage): add localization to NotFoundPage

--- src/router/pages/NotFoundPage/NotFoundPage.tsx
+++ src/router/pages/NotFoundPage/NotFoundPage.tsx
@@ -1,30 +1,33 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { useTranslation } from 'react-i18next';
 import Title from '../../../components/UI/Title/Title';
 import classes from './NotFoundPage.module.css';
 
-const NotFoundPage = () => (
-  <div
-    className={classes['not-found']}
-  >
-    <div className={`${classes['not-found__container']} container`}>
-      <Title
-        className={classes['not-found__title']}
-        level={1}
-        component='h1'
-      >
-        <span>404</span>
-        Страница не найдена
-      </Title>
-      <Link
-        className={classes['not-found__link']}
-        to='/books'
-      >
-        Вернуться на главную страницу
-      </Link>
+const NotFoundPage = () => {
+  const { t } = useTranslation();
+  return (
+    <div
+      className={classes['not-found']}
+    >
+      <div className={`${classes['not-found__container']} container`}>
+        <Title
+          className={classes['not-found__title']}
+          level={1}
+          component='h1'
+        >
+          <span>404</span>
+          {t('mainTitle', { ns: 'notFoundPage' })}
+        </Title>
+        <Link
+          className={classes['not-found__link']}
+          to='/books'
+        >
+          {t('backToHomePage', { ns: 'notFoundPage' })}
+        </Link>
+      </div>
     </div>
-
-  </div >
-);
+  );
+};
 
 export default NotFoundPage;
