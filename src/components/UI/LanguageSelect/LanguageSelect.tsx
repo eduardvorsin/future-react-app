57c950311de35541e16548d900a09a4d2f563b1c
eduardvorsin <eduardvorsin2019@yandex.kz>
2023-07-15 13:55:06 +0600

    feat(languageselect): create a new component

---
+++ src/components/UI/LanguageSelect/LanguageSelect.tsx
@@ -0,0 +1,55 @@
+import React, { ChangeEventHandler, FC, useContext, useState } from 'react';
+import Select from '../Select/Select';
+import classes from './LanguageSelect.module.css';
+import i18n from '../../../localization/i18next';
+import { LanguageContext, Languages } from '../../../contexts/LanguageContext';
+
+type LanguageOption = {
+  value: Languages,
+  label: string,
+}
+
+const languagesOptions: LanguageOption[] = [
+  {
+    value: 'en',
+    label: 'ENG',
+  },
+  {
+    value: 'ru',
+    label: 'RUS',
+  },
+];
+
+type LanguageSelect = {
+  className?: string,
+  onChange?: ChangeEventHandler<HTMLSelectElement>,
+}
+
+const LanguageSelect: FC<LanguageSelect> = ({
+  className,
+  onChange,
+}) => {
+  const language = useContext(LanguageContext);
+  const [selectValue, setSelectValue] = useState<string>(language.value);
+  const languageChangeHandler: ChangeEventHandler<HTMLSelectElement> = (e) => {
+    const currentLanguage = e.target.value as Languages;
+    setSelectValue(currentLanguage);
+    i18n.changeLanguage(currentLanguage);
+    language.selectLanguage(currentLanguage);
+
+    if (onChange) onChange(e);
+  };
+
+  return (
+    <Select
+      id='language-select'
+      className={`${classes.language__select} ${className}`}
+      labelText='language selection'
+      options={languagesOptions}
+      value={selectValue}
+      onChange={languageChangeHandler}
+    />
+  );
+};
+
+export default LanguageSelect;
