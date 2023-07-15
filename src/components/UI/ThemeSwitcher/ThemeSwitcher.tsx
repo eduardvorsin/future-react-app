2fe8a3dc8db9366435b5ae5d5ca03a1f3065ea9e
eduardvorsin <eduardvorsin2019@yandex.kz>
2023-07-15 13:57:17 +0600

    feat(themeswitcher): add localization to the component

--- src/components/UI/ThemeSwitcher/ThemeSwitcher.tsx
+++ src/components/UI/ThemeSwitcher/ThemeSwitcher.tsx
@@ -1,4 +1,5 @@
 import React, { ChangeEventHandler, FC, useContext, useState } from 'react';
+import { useTranslation } from 'react-i18next';
 import Switcher from '../Switcher/Switcher';
 import classes from './ThemeSwitcher.module.css';
 import { ThemeContext } from '../../../contexts/ThemeContext';
@@ -10,6 +11,7 @@ type ThemeSwitcherProps = {
 const ThemeSwitcher: FC<ThemeSwitcherProps> = ({
   className,
 }) => {
+  const { t } = useTranslation();
   const theme = useContext(ThemeContext);
   const [checked, setChecked] = useState<boolean>(theme.value === 'light');
 
@@ -18,15 +20,17 @@ const ThemeSwitcher: FC<ThemeSwitcherProps> = ({
     setChecked(!checked);
   };
 
-  const themeValues = ['light', 'dark'];
+  const themeValues: [string, string] = ['light', 'dark'];
+  const labels: [string, string] = ['светлая тема', 'темная тема'];
 
   return (
     <Switcher
-      labelText='color theme switcher'
+      labelText={t('themeSwitcherLabel')}
       name='theme-switcher'
       onChange={themeChangeHandler}
       checked={checked}
       className={`${classes['theme-switcher']} ${className}`}
+      inputLabels={labels}
       value={themeValues}
     />
   );
