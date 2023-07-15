3e21702e07b2271b79d4f0301436b25ac09f669f
eduardvorsin <eduardvorsin2019@yandex.kz>
2023-07-15 14:04:03 +0600

    feat(booktypes): move the fields and the limit to separate variables and add a language for queries

--- src/API/BooksAPI.ts
+++ src/API/BooksAPI.ts
@@ -1,4 +1,5 @@
 import { getPageIndex } from '../helpers/helpers';
+import i18n from '../localization/i18next';
 import { SearchOptions } from './bookTypes';
 
 export default class BooksAPI {
@@ -16,24 +17,29 @@ export default class BooksAPI {
     const currentCategory = category === 'all' ? '' : `+subject:${category}`;
     const startIndex = getPageIndex(page);
 
+    const limit = 40;
+    const fields = 'totalItems,items(id,volumeInfo/authors,volumeInfo/title,volumeInfo/imageLinks,volumeInfo/maturityRating,saleInfo/retailPrice)';
+
     const searchParams = [
       `q=${searchBy}:${bookName}${currentCategory}`,
       `orderBy=${sortOrder}`,
-      'fields=totalItems,items(id,volumeInfo/authors,volumeInfo/title,volumeInfo/imageLinks,volumeInfo/maturityRating,saleInfo/retailPrice)',
-      'langRestrict=ru',
-      'maxResults=40',
+      `fields=${fields}`,
+      `langRestrict=${i18n.language}`,
+      `maxResults=${limit}`,
       `startIndex=${startIndex}`,
       `key=${process.env.API_KEY as string}`,
     ].join('&');
 
     const response = await fetch(`${BooksAPI.url}?${searchParams}`);
-
     return response;
   }
 
-  static async getDefiniteBook(id: string) {
+  static async getDefiniteBook(id: string): Promise<Response> {
+    const fields = 'id,volumeInfo(authors,title,description,categories,imageLinks,language,maturityRating,pageCount,publishedDate,publisher,industryIdentifiers),accessInfo(webReaderLink),saleInfo(buyLink,isEbook,retailPrice)';
+
     const searchParams = [
-      'fields=id,volumeInfo(authors,title,description,categories,imageLinks,language,maturityRating,pageCount,publishedDate,publisher,industryIdentifiers),accessInfo(webReaderLink),saleInfo(buyLink,isEbook,retailPrice)',
+      `fields=${fields}`,
+      `langRestrict=${i18n.language}`,
       `key=${process.env.API_KEY as string}`,
     ].join('&');
 
