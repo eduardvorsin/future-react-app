179f05f6d91c99707f457a64d02f5345fd3262d4
eduardvorsin <eduardvorsin2019@yandex.kz>
2023-07-22 13:50:50 +0600

    feat(bookdescription): replace fetchDefiniteBook with query from Books API

--- src/router/loaders/bookDescriptionLoader.ts
+++ src/router/loaders/bookDescriptionLoader.ts
@@ -1,18 +1,28 @@
 import { LoaderFunctionArgs, ParamParseKey, Params } from 'react-router-dom';
-import fetchDefiniteBook from '../../store/thunks/fetchDefiniteBook/fetchDefiniteBook';
 import { bookDescriptionPathName } from '../AppRouter/AppRouter';
 import store from '../../store/store';
+import { BooksAPI } from '../../API/BooksAPI';
+import i18n from '../../localization/i18next';
+import { IBook } from '../../model/IBook';
 
 interface bookDescriptionLoaderArgs extends LoaderFunctionArgs {
   params: Params<ParamParseKey<bookDescriptionPathName>>
 }
 
-const bookDescriptionLoader = async ({ params }: bookDescriptionLoaderArgs): Promise<null> => {
-  if (params.id) {
-    store.dispatch(fetchDefiniteBook(params.id));
-  }
+// eslint-disable-next-line max-len
+const bookDescriptionLoader = async ({ params }: bookDescriptionLoaderArgs): Promise<IBook | null> => {
+  if (!params.id) return null;
 
-  return null;
+  const promise = store.dispatch(BooksAPI.endpoints.getBookById.initiate(params.id));
+
+  try {
+    const response = await promise.unwrap();
+    return response;
+  } catch (e) {
+    throw new Error(i18n.t('fetchDefiniteBookError'));
+  } finally {
+    promise.unsubscribe();
+  }
 };
 
 export default bookDescriptionLoader;
