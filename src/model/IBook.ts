7262b5f62ece4da96a237829856828ea0bac115a
eduardvorsin <eduardvorsin2019@yandex.kz>
2023-07-22 14:02:23 +0600

    feat(ibook): add the ClientID field to the IBookPartial type

--- src/model/IBook.ts
+++ src/model/IBook.ts
@@ -3,5 +3,8 @@ import { AccessInfo, SaleInfo, VolumeInfo } from '../API/bookTypes';
 export type IBook = VolumeInfo & AccessInfo & SaleInfo;
 
 export type IBookPartial =
-  { id: string } &
+  {
+    id: string,
+    clientId: string,
+  } &
   Pick<VolumeInfo, 'authors' | 'maturityRating' | 'title' | 'imageLinks'> & Pick<SaleInfo, 'retailPrice'>;
