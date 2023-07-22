605d731b13268c70b37f0bce96bff4edde1d7f2a
eduardvorsin <eduardvorsin2019@yandex.kz>
2023-07-22 13:46:51 +0600

    feat(booksapi): replace its interface with a class for accessing the Google Books API with RTK query

--- src/API/BooksAPI.ts
+++ src/API/BooksAPI.ts
@@ -1,49 +1,119 @@
+/* eslint-disable no-param-reassign */
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { v4 as uuidv4 } from 'uuid';
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 import { getPageIndex } from '../helpers/helpers';
 import i18n from '../localization/i18next';
-import { SearchOptions } from './bookTypes';
+import { BookInfo, BookPartialInfo, SearchOptions } from './bookTypes';
+import { IBook, IBookPartial } from '../model/IBook';
 
-export default class BooksAPI {
-  private static url = 'https://www.googleapis.com/books/v1/volumes';
+type BooksQueryData = {
+  totalItems: number,
+  items: IBookPartial[],
+};
 
-  static async getBooks(options: SearchOptions): Promise<Response> {
-    const {
-      bookName,
-      sortOrder = 'relevance',
-      category = 'all',
-      page,
-      searchBy = 'intitle',
-    } = options;
+type GetBooksResponse = {
+  totalItems: number,
+  items: BookPartialInfo[],
+};
 
-    const currentCategory = category === 'all' ? '' : `+subject:${category}`;
-    const startIndex = getPageIndex(page);
+const booksPerPageLimit = 40;
 
-    const limit = 40;
-    const fields = 'totalItems,items(id,volumeInfo/authors,volumeInfo/title,volumeInfo/imageLinks,volumeInfo/maturityRating,saleInfo/retailPrice)';
+export const BooksAPI = createApi({
+  reducerPath: 'booksApi',
+  baseQuery: fetchBaseQuery({ baseUrl: 'https://www.googleapis.com/books/v1/' }),
+  tagTypes: ['Books', 'Book'],
+  endpoints: (builder) => ({
+    getBooks: builder.query<BooksQueryData, SearchOptions>({
+      query: (options) => {
+        const {
+          bookName,
+          sortOrder = 'relevance',
+          category = 'all',
+          page,
+          searchBy = 'intitle',
+        } = options;
 
-    const searchParams = [
-      `q=${searchBy}:${bookName}${currentCategory}`,
-      `orderBy=${sortOrder}`,
-      `fields=${fields}`,
-      `langRestrict=${i18n.language}`,
-      `maxResults=${limit}`,
-      `startIndex=${startIndex}`,
-      `key=${process.env.API_KEY as string}`,
-    ].join('&');
+        const currentCategory = category === 'all' ? '' : `+subject:${category}`;
+        const startIndex = getPageIndex(page);
+        const fields = 'totalItems,items(id,volumeInfo/authors,volumeInfo/title,volumeInfo/imageLinks,volumeInfo/maturityRating,saleInfo/retailPrice)';
 
-    const response = await fetch(`${BooksAPI.url}?${searchParams}`);
-    return response;
-  }
+        return {
+          url: 'volumes',
+          params: {
+            q: `${searchBy}:${bookName}${currentCategory}`,
+            orderBy: `${sortOrder}`,
+            fields: `${fields}`,
+            langRestrict: `${i18n.language}`,
+            maxResults: `${booksPerPageLimit}`,
+            startIndex: `${startIndex}`,
+            key: `${process.env.API_KEY as string}`,
+          },
+        };
+      },
+      serializeQueryArgs: ({ endpointName }) => endpointName,
+      merge: (currentCacheData, responseData, otherArgs) => {
+        if (otherArgs.arg.page > 0) {
+          currentCacheData.items.push(...responseData.items);
+          currentCacheData.totalItems = responseData.totalItems;
+        } else {
+          currentCacheData.items = responseData.items;
+          currentCacheData.totalItems = responseData.totalItems;
+        }
+      },
+      forceRefetch({ currentArg, previousArg }) {
+        if (!currentArg || !previousArg) return true;
 
-  static async getDefiniteBook(id: string): Promise<Response> {
-    const fields = 'id,volumeInfo(authors,title,description,categories,imageLinks,language,maturityRating,pageCount,publishedDate,publisher,industryIdentifiers),accessInfo(webReaderLink),saleInfo(buyLink,isEbook,retailPrice)';
+        const keys = Object.keys(currentArg ?? {}) as Array<keyof typeof currentArg>;
+        const shouldRefetch = keys.some((key) => currentArg?.[key] !== previousArg?.[key]);
 
-    const searchParams = [
-      `fields=${fields}`,
-      `langRestrict=${i18n.language}`,
-      `key=${process.env.API_KEY as string}`,
-    ].join('&');
+        return shouldRefetch;
+      },
+      transformResponse: (response: GetBooksResponse) => {
+        const transformedData = {
+          totalItems: response.totalItems,
+        } as BooksQueryData;
 
-    const response = await fetch(`${BooksAPI.url}/${id}?${searchParams}`);
-    return response;
-  }
-}
+        if (!response.items || !response.items.length) {
+          transformedData.items = [];
+        } else {
+          transformedData.items = response.items.map((item) => ({
+            id: item.id,
+            clientId: uuidv4(),
+            ...item.saleInfo,
+            ...item.volumeInfo,
+          }));
+        }
+
+        return transformedData;
+      },
+      providesTags: ['Books'],
+    }),
+    getBookById: builder.query<IBook, string>({
+      query: (id) => {
+        const fields = 'id,volumeInfo(authors,title,description,categories,imageLinks,language,maturityRating,pageCount,publishedDate,publisher,industryIdentifiers),accessInfo(webReaderLink),saleInfo(buyLink,isEbook,retailPrice)';
+
+        return {
+          url: `volumes/ ${id}`,
+          params: {
+            fields: `${fields}`,
+            langRestrict: `${i18n.language}`,
+            key: `${process.env.API_KEY as string}`,
+          },
+        };
+      },
+      transformResponse: (response: BookInfo) => {
+        const transformedData: IBook = {
+          ...response.volumeInfo,
+          ...response.saleInfo,
+          ...response.accessInfo,
+        };
+
+        return transformedData;
+      },
+      providesTags: ['Book'],
+    }),
+  }),
+});
+
+export const { useGetBooksQuery, useGetBookByIdQuery } = BooksAPI;
