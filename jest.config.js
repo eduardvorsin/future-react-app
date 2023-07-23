9d9e184f2f7da9bcce1b29d097864a33379b867f
eduardvorsin <eduardvorsin2019@yandex.kz>
2023-07-23 13:58:56 +0600

    test(package.json/jest.config): fix a bug when classes from css modules were not used in tests

--- jest.config.js
+++ jest.config.js
@@ -27,7 +27,7 @@ module.exports = {
 
   moduleNameMapper: {
     "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$": "<rootDir>/src/tests/__mocks__/fileMock.ts",
-    "\\.(css|scss|sass)$": "<rootDir>/src/tests/__mocks__/styleMock.ts",
+    "\\.(css|scss|sass)$": "identity-obj-proxy",
   },
 
   testMatch: [
