8fbcff8d6c6b8528057c8ad877161a83b5baec78
eduardvorsin <eduardvorsin2019@yandex.kz>
2023-07-30 14:36:11 +0600

    test(setup-jest): added an mock for matchMedia

--- setup-jest.js
+++ setup-jest.js
@@ -1,1 +1,12 @@
 import '@testing-library/jest-dom';
+
+Object.defineProperty(window, 'matchMedia', {
+  writable: true,
+  value: (query) => ({
+    matches: true,
+    media: query,
+    onchange: null,
+    addListener: jest.fn(),
+    removeListener: jest.fn(),
+  }),
+});
