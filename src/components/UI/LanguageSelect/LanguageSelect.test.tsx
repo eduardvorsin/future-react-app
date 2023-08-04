4797806b3bdde310ab4b045e2a1a83bb557892e6
eduardvorsin <eduardvorsin2019@yandex.kz>
2023-08-04 15:05:20 +0600

    test(languageselect.test): remove unnecessary mock and provider for i18next

--- src/components/UI/LanguageSelect/LanguageSelect.test.tsx
+++ src/components/UI/LanguageSelect/LanguageSelect.test.tsx
@@ -2,23 +2,10 @@
 import React, { ChangeEvent } from 'react';
 import { render, screen, waitFor } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
-import { I18nextProvider } from 'react-i18next';
 import LanguageSelect from './LanguageSelect';
 import i18n from '../../../localization/i18next';
 
 describe('LanguageSelect tests', () => {
-  beforeAll(() => {
-    jest.mock('react-i18next', () => ({
-      ...jest.requireActual('react-i18next'),
-      useTranslation: () => ({
-        t: (str: string) => str,
-        i18n: {
-          changeLanguage: () => new Promise(() => { }),
-        },
-      }),
-    }));
-  });
-
   it('is rendered correctly', () => {
     render(<LanguageSelect />);
 
@@ -52,11 +39,7 @@ describe('LanguageSelect integration tests', () => {
   it('the component with the text in English is displayed correctly', () => {
     const localizedText = i18n.t('languageSelection');
 
-    render(
-      <I18nextProvider i18n={i18n}>
-        <LanguageSelect />
-      </I18nextProvider>,
-    );
+    render(<LanguageSelect />);
 
     expect(screen.getByLabelText<HTMLDivElement>(localizedText)).toBeInTheDocument();
   });
@@ -65,11 +48,7 @@ describe('LanguageSelect integration tests', () => {
     i18n.changeLanguage('ru');
     const localizedText = i18n.t('languageSelection');
 
-    render(
-      <I18nextProvider i18n={i18n}>
-        <LanguageSelect />
-      </I18nextProvider>,
-    );
+    render(<LanguageSelect />);
 
     expect(screen.getByLabelText<HTMLDivElement>(localizedText)).toBeInTheDocument();
   });
