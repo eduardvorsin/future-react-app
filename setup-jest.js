b52cf96125a566efae4f253cdd1c185efdbf00e3
eduardvorsin <eduardvorsin2019@yandex.kz>
2023-08-04 14:51:20 +0600

    test(setup-jest): configure cross-fetch and msw for jest tests

--- setup-jest.js
+++ setup-jest.js
@@ -1,4 +1,6 @@
 import '@testing-library/jest-dom';
+import { fetch, Headers, Request, Response } from 'cross-fetch';
+import server from './src/tests/server';
 
 Object.defineProperty(window, 'matchMedia', {
   writable: true,
@@ -9,4 +11,21 @@ Object.defineProperty(window, 'matchMedia', {
     addListener: jest.fn(),
     removeListener: jest.fn(),
   }),
+});
+
+global.fetch = fetch;
+global.Headers = Headers;
+global.Request = Request;
+global.Response = Response;
+
+beforeAll(() => {
+  server.listen();
+});
+
+afterEach(() => {
+  server.resetHandlers();
+});
+
+afterAll(() => {
+  server.close();
 });
