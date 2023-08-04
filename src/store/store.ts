3b230e629664b61ff0ef5972e49a6f4acfea08e2
eduardvorsin <eduardvorsin2019@yandex.kz>
2023-08-04 14:52:25 +0600

    feat(store): add the ability to transfer the initial state to the setupStore

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,19 +1,20 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { PreloadedState, combineReducers, configureStore } from '@reduxjs/toolkit';
 import { setupListeners } from '@reduxjs/toolkit/dist/query';
 import { BooksAPI } from '../API/BooksAPI/BooksAPI';
 
-const setupStore = () => configureStore({
-  reducer: {
-    [BooksAPI.reducerPath]: BooksAPI.reducer,
-  },
+const rootReducer = combineReducers({
+  [BooksAPI.reducerPath]: BooksAPI.reducer,
+});
+
+export type RootState = ReturnType<typeof rootReducer>;
+export const setupStore = (preloadedState?: PreloadedState<RootState>) => configureStore({
+  reducer: rootReducer,
+  preloadedState,
   middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(BooksAPI.middleware),
 });
 
 const store = setupStore();
-
 setupListeners(store.dispatch);
 
-export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
-
 export default store;
