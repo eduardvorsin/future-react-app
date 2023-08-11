41c2829aa7afa761af4de9e7715222c1f8f8209a
eduardvorsin <eduardvorsin2019@yandex.kz>
2023-08-11 11:48:18 +0600

    feat(notfoundpage): add a testID prop

--- src/router/pages/NotFoundPage/NotFoundPage.tsx
+++ src/router/pages/NotFoundPage/NotFoundPage.tsx
@@ -1,14 +1,21 @@
-import React from 'react';
+import React, { FC } from 'react';
 import { Link } from 'react-router-dom';
 import { useTranslation } from 'react-i18next';
 import Title from '../../../components/UI/Title/Title';
 import classes from './NotFoundPage.module.css';
 
-const NotFoundPage = () => {
+type NotFoundPageProps = {
+  testId?: string,
+};
+
+const NotFoundPage: FC<NotFoundPageProps> = ({
+  testId,
+}) => {
   const { t } = useTranslation();
   return (
     <div
       className={classes['not-found']}
+      data-testid={testId}
     >
       <div className={`${classes['not-found__container']} container`}>
         <Title
