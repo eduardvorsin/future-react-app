ffdea680d8c0af7625fe691e9cb5bf667caa4101
eduardvorsin <eduardvorsin2019@yandex.kz>
2023-08-12 11:49:16 +0600

    feat(app): take out the logic of providers into separate components and connect them to the App

--- src/App.tsx
+++ src/App.tsx
@@ -1,43 +1,14 @@
-import React, { useCallback, useEffect, useMemo, useState } from 'react';
+import React from 'react';
 import AppRouter from './router/AppRouter/AppRouter';
-import { Theme, ThemeContext, defaultTheme } from './contexts/ThemeContext';
-import { LanguageContext, Languages } from './contexts/LanguageContext';
-import i18n from './localization/i18next';
+import ThemeProvider from './components/UI/ThemeProvider/ThemeProvider';
+import LanguageProvider from './components/UI/LanguageProvider/LanguageProvider';
 
 export default function App() {
-  const [theme, setTheme] = useState<Theme>(defaultTheme);
-  const [language, setLanguage] = useState<Languages>(i18n.language as Languages);
-
-  const toggleTheme: () => void = useCallback(() => {
-    setTheme((prevTheme) => (prevTheme === 'light' ? 'dark' : 'light'));
-  }, [theme]);
-
-  const selectLanguage: (currentLanguage: Languages) => void = useCallback((currentLanguage) => {
-    setLanguage(currentLanguage);
-  }, [language]);
-
-  const themeContextValue = useMemo(() => ({
-    value: theme,
-    toggleTheme,
-  }), [theme, toggleTheme]);
-
-  const languageContextValue = useMemo(() => ({
-    value: language,
-    selectLanguage,
-  }), [language, selectLanguage]);
-
-  useEffect(() => {
-    document.documentElement.setAttribute('data-theme', theme);
-    return () => {
-      document.documentElement.removeAttribute('data-theme');
-    };
-  }, [theme]);
-
   return (
-    <ThemeContext.Provider value={themeContextValue}>
-      <LanguageContext.Provider value={languageContextValue}>
+    <ThemeProvider>
+      <LanguageProvider>
         <AppRouter />
-      </LanguageContext.Provider>
-    </ThemeContext.Provider>
+      </LanguageProvider>
+    </ThemeProvider>
   );
 }
